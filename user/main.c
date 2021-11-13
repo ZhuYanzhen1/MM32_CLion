ce557174817ed0aa0416e035980eafa97f86c479
Lao·Zhu <2208213223@qq.com>
2021-11-14 00:22:57 +0800

    modify incorrect header file.

--- user/main.c
+++ user/main.c
@@ -8,10 +8,7 @@ int main(void) {
     led_config();
     UART1_NVIC_Init(115200);
     while (1) {
-        LED1_ON();
-        delayms(5);
-        LED1_OFF();
-        delayms(5);
-        //UART1_RxTx_Transceiving();
+        LED1_TOGGLE();
+        delayms(500);
     }
 }
