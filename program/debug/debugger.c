03189d0977f780882e61b52fe6976520746e871a
Lao·Zhu <2208213223@qq.com>
2021-12-06 00:16:58 +0800

    Add code for Debugger to register variables.

--- program/debug/debugger.c
+++ program/debug/debugger.c
@@ -7,6 +7,13 @@
 ******************************************************************************/
 
 #include "debugger.h"
+#include "config.h"
+#include "encrypt.h"
+#include "malloc.h"
+#include "string.h"
+
+static debugger_variable_t *variable_buffer[VARIABLE_BUFFER_SIZE];
+static unsigned char variable_index = 0;
 
 /*!
     \brief        user callback function for unpacking completion of medium capacity transport protocol
@@ -15,4 +22,18 @@
     \retval none
 */
 void mdtp_callback_handler(unsigned char pid, const unsigned char *data) {
+}
+
+void debugger_register_variable(variable_type var_type, void *variable, const char *name) {
+    unsigned char name_length = strlen(name) > 16 ? 16 : strlen(name);
+    debugger_variable_t *new_variable = memalloc(sizeof(debugger_variable_t));
+    new_variable->var_status = var_type;
+    new_variable->var_address = variable;
+    for (unsigned char counter = 0; counter < name_length; counter++)
+        new_variable->var_name[counter] = name[counter];
+    variable_buffer[variable_index] = new_variable;
+    variable_index = variable_index + 1;
+}
+
+void debugger_scan_variable(unsigned long time_stamp) {
 }
--- program/debug/debugger.h
+++ program/debug/debugger.h
@@ -9,6 +9,20 @@
 #ifndef MM32F3277_DEBUG_DEBUGGER_H_
 #define MM32F3277_DEBUG_DEBUGGER_H_
 
+typedef enum {
+    integer_8bit = 0,
+    integer_16bit = 1,
+    integer_32bit = 2,
+    float_32bit = 3
+} variable_type;
+
+typedef struct DEBUGGER_VARIABLE_T {
+    unsigned int var_status;
+    void *var_address;
+    unsigned char var_name[16];
+} debugger_variable_t;
+
 void mdtp_callback_handler(unsigned char pid, const unsigned char *data);
+void debugger_register_variable(variable_type var_type, void *variable, const char *name);
 
 #endif  //MM32F3277_DEBUG_DEBUGGER_H_
