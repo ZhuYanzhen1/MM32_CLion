4bd2394bd6e0827e2952972277d844c32531662b
Lao·Zhu <2208213223@qq.com>
2021-12-09 11:21:52 +0800

    Add serial port object to monitor's code and port mdtp codec protocol.

--- tools/mainwindow.cpp
+++ tools/mainwindow.cpp
@@ -3,13 +3,19 @@
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+    , ui(new Ui::MainWindow) {
+    serial = new QSerialPort();
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    if (serial->isOpen())
+        serial->close();
+    delete serial;
     delete ui;
 }
 
+void MainWindow::mdtp_callback_handler(unsigned char pid, const unsigned char *data) {
+    (void) pid;
+    (void) data;
+}
--- tools/mainwindow.h
+++ tools/mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QMessageBox>
+#include <QTextEdit>
+#include <QSlider>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -17,5 +22,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
+    QSerialPort *serial;
+    void mdtp_data_transmit(unsigned char pid, const unsigned char *data);
+    void mdtp_receive_handler(unsigned char data);
+    void mdtp_callback_handler(unsigned char pid, const unsigned char *data);
 };
+
 #endif // MAINWINDOW_H
