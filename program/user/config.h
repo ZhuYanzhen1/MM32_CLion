15a013f188f969e30d8ebe353ac9648f2fb6c71f
Lao·Zhu <2208213223@qq.com>
2021-12-18 16:38:33 +0800

    Import the cmbacktrace source file and remove the clang-format configuration file.

--- program/user/config.h
+++ program/user/config.h
@@ -15,4 +15,17 @@
 #define common_sendbyte uart1_sendbyte
 #define configTOTAL_HEAP_SIZE ((unsigned int) (10 * 1024))
 
+/* enable bare metal(no OS) platform */
+/* #define CMB_USING_BARE_METAL_PLATFORM */
+/* enable OS platform */
+/* #define CMB_USING_OS_PLATFORM */
+/* OS platform type, must config when CMB_USING_OS_PLATFORM is enable */
+/* #define CMB_OS_PLATFORM_TYPE           CMB_OS_PLATFORM_RTT or CMB_OS_PLATFORM_UCOSII or CMB_OS_PLATFORM_UCOSIII or CMB_OS_PLATFORM_FREERTOS */
+/* cpu platform type, must config by user */
+#define CMB_CPU_PLATFORM_TYPE          /* CMB_CPU_ARM_CORTEX_M0 or CMB_CPU_ARM_CORTEX_M3 or CMB_CPU_ARM_CORTEX_M4 or CMB_CPU_ARM_CORTEX_M7 */
+/* enable dump stack information */
+/* #define CMB_USING_DUMP_STACK_INFO */
+/* language of print information */
+/* #define CMB_PRINT_LANGUAGE             CMB_PRINT_LANGUAGE_ENGLISH(default) or CMB_PRINT_LANGUAGE_CHINESE */
+
 #endif  // MM32F3277_USER_CONFIG_H_
