e9a162dff2542c752b902891c890b66e9b440c62
zgl <1662568794@qq.com>
2021-12-24 17:16:10 +0800

    gps text.

--- testbench/script/gps_text.c
+++ testbench/script/gps_text.c
@@ -9,17 +9,17 @@
 void test_nmea_comma_position() {
     char data[] = "$GPGGA,235316.000,3959.9925,S,12000.0090,E,1,06,1.21,62.77,M,0.00,M,,*7B";
     char *p = data;
-    char n1 = nmea_comma_position(data, 2);
+    int n1 = nmea_comma_position(data, 2);
     CU_ASSERT_EQUAL(*(p + n1), '3'); //TURE
     CU_ASSERT_EQUAL(*(p + n1), 'J'); //FAULT
     CU_ASSERT_EQUAL(n1, 7); //TURE
     CU_ASSERT_EQUAL(n1, 6); //FAULT
-    char n2 = nmea_comma_position(data, 5);
+    int n2 = nmea_comma_position(data, 5);
     CU_ASSERT_EQUAL(*(p + n2), 'E'); //TURE
     CU_ASSERT_EQUAL(*(p + n2), '0'); //FAULT
     CU_ASSERT_EQUAL(n2, 18); //TURE
     CU_ASSERT_EQUAL(n2, 19); //FAULT
-    char n3 = nmea_comma_position(data, 20);
+    int n3 = nmea_comma_position(data, 20);
     CU_ASSERT_EQUAL(*(p + n3), '0'); //FAULT
     CU_ASSERT_EQUAL(n3, 0XFE); //TURE
     CU_ASSERT_EQUAL(n3, sizeof(data)); //TURE
