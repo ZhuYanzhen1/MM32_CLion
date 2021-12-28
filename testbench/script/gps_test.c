7b05627d4bdb55fb17d916c83cf80ef64b6fafa7
Lao·Zhu <2208213223@qq.com>
2021-12-28 14:46:01 +0800

    Eliminate error warnings when using memalloc.

--- testbench/script/gps_test.c
+++ testbench/script/gps_test.c
@@ -51,12 +51,11 @@ void test_nmea_str2num() {
     CU_ASSERT_NOT_EQUAL(nmea_str2num(p[3], d), -13.424);
     CU_ASSERT_NOT_EQUAL(nmea_str2num(p[4], d), 0.134);
 
-
 }
 
 void test_nmea_gpgga_analysis() {
     char data[] = "$GPGGA,235316.000,3959.9925,S,12000.0090,E,1,06,1.21,62.77,M,0.00,M,,*7B";
-    nmea_gga *gps_gga = malloc(sizeof(nmea_gga));
+    nmea_gga *gps_gga = memalloc(sizeof(nmea_gga));
     nmea_gpgga_analysis(gps_gga, data);
     CU_ASSERT_EQUAL(gps_gga->latitude_direction, 'S');
     CU_ASSERT_EQUAL(gps_gga->checksum, 0X7B);
@@ -66,7 +65,7 @@ void test_nmea_gpgga_analysis() {
 
 void test_nmea_gpant_analysis() {
     char data[] = "$GPTXT,01,01,01,ANTENNA SHORT*63";
-    nmea_ant *gps_ant = malloc(sizeof(nmea_ant));
+    nmea_ant *gps_ant = memalloc(sizeof(nmea_ant));
     nema_gpant_analysis(gps_ant, data);
     CU_ASSERT_EQUAL(gps_ant->xx, 1);
     CU_ASSERT_EQUAL(gps_ant->yy, 1);
