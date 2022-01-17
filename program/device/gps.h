d9279f0a1e4c48504be28df5e867e44ae2505f99
zgl <1662568794@qq.com>
2022-01-17 22:21:13 +0800

    State machine to resolve truncated gps packets due to DMA reads

---
+++ program/device/gps.h
@@ -0,0 +1,13 @@
+//
+// Created by 16625 on 2022-01-16.
+//
+
+#ifndef MAIN_C_DEVICE_GPS_H_
+#define MAIN_C_DEVICE_GPS_H_
+
+#include "gps_parser.h"
+
+void gps_config();
+void gui_show_gnrmc_information(nmea_rmc *gps_rmc);
+
+#endif //MAIN_C_DEVICE_GPS_H_
