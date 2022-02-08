39047d3940fe5a1903c33ca3ecd83cd2bf46b7f9
Lao·Zhu <2208213223@qq.com>
2022-02-09 01:46:26 +0800

    Complete the discrete Riccati equation iteration code, which works pretty well.

--- testbench/riccati_solver/riccati_solver.c
+++ testbench/riccati_solver/riccati_solver.c
@@ -1,36 +1,144 @@
 #include "riccati_solver.h"
 #include "stdio.h"
+#include "math.h"
 
-//  P = 1.5011    1.0602   -0.0839
-//      1.0602    9.3531    0.2382
-//      -0.0839    0.2382    0.0994
+//  P = 96.7181   16.2375   -2.0601
+//      16.2375   31.0814    8.2303
+//      -2.0601    8.2303   11.3719
+
+#define ITERATION_ACCURACY  0.001
 
 void solveRiccatiIteration(double A[3][3], double B[3][2], double Q[3][3], double R[2][2]) {
-    printf("\r\n------------Matrix A------------\r\n");
+    double matrix_p_next[3][3] = {0}, matrix_p[3][3] = {{Q[0][0], Q[0][1], Q[0][2]},
+                                                        {Q[1][0], Q[1][1], Q[1][2]},
+                                                        {Q[2][0], Q[2][1], Q[2][2]}};
+    for (unsigned int counter = 0; counter < 100000; ++counter) {
+        double matrix_at_pn_a[3][3] =
+            {{matrix_p[0][0], matrix_p[0][1], A[0][2] * matrix_p[0][0] + A[1][2] * matrix_p[0][1] + matrix_p[0][2]},
+             {matrix_p[1][0], matrix_p[1][1], A[0][2] * matrix_p[1][0] + A[1][2] * matrix_p[1][1] + matrix_p[1][2]},
+             {A[0][2] * matrix_p[0][0] + A[1][2] * matrix_p[1][0] + matrix_p[2][0],
+              A[0][2] * matrix_p[0][1] + A[1][2] * matrix_p[1][1] + matrix_p[2][1],
+              A[0][2] * matrix_p[0][2]
+                  + A[1][2] * matrix_p[1][2]
+                  + A[0][2] * (A[0][2] * matrix_p[0][0] + A[1][2] * matrix_p[1][0] + matrix_p[2][0])
+                  + A[1][2] * (A[0][2] * matrix_p[0][1] + A[1][2] * matrix_p[1][1] + matrix_p[2][1]) + matrix_p[2][2]}};
+        double
+            matrix_at_pn_b[3][2] =
+            {{B[0][0] * matrix_p[0][0] + B[1][0] * matrix_p[0][1] + B[2][0] * matrix_p[0][2], B[2][1] * matrix_p[0][2]},
+             {B[0][0] * matrix_p[1][0] + B[1][0] * matrix_p[1][1] + B[2][0] * matrix_p[1][2], B[2][1] * matrix_p[1][2]},
+             {B[0][0] * (A[0][2] * matrix_p[0][0] + A[1][2] * matrix_p[1][0] + matrix_p[2][0])
+                  + B[1][0] * (A[0][2] * matrix_p[0][1] + A[1][2] * matrix_p[1][1] + matrix_p[2][1])
+                  + B[2][0] * (A[0][2] * matrix_p[0][2] + A[1][2] * matrix_p[1][2] + matrix_p[2][2]),
+              B[2][1] * (A[0][2] * matrix_p[0][2] + A[1][2] * matrix_p[1][2] + matrix_p[2][2])}};
+        double matrix_r_bt_pn_b[2][2] =
+            {{B[0][0] * (B[0][0] * matrix_p[0][0] + B[1][0] * matrix_p[1][0]
+                + B[2][0] * matrix_p[2][0]) + B[1][0] * (B[0][0] * matrix_p[0][1] + B[1][0] * matrix_p[1][1]
+                + B[2][0] * matrix_p[2][1])
+                  + B[2][0] * (B[0][0] * matrix_p[0][2] + B[1][0] * matrix_p[1][2] + B[2][0] * matrix_p[2][2])
+                  + R[0][0],
+              B[2][1] * (B[0][0] * matrix_p[0][2] + B[1][0] * matrix_p[1][2] + B[2][0] * matrix_p[2][2])},
+             {B[0][0] * B[2][1] * matrix_p[2][0]
+                  + B[1][0] * B[2][1] * matrix_p[2][1] + B[2][0] * B[2][1] * matrix_p[2][2],
+              B[2][1] * B[2][1] * matrix_p[2][2] + R[0][0]}};
+        double
+            matrix_bt_pn_a[2][3] =
+            {{B[0][0] * matrix_p[0][0] + B[1][0] * matrix_p[1][0]
+                  + B[2][0] * matrix_p[2][0],
+              B[0][0] * matrix_p[0][1] + B[1][0] * matrix_p[1][1] + B[2][0] * matrix_p[2][1],
+              B[0][0] * matrix_p[0][2]
+                  + B[1][0] * matrix_p[1][2]
+                  + A[0][2] * (B[0][0] * matrix_p[0][0]
+                      + B[1][0] * matrix_p[1][0]
+                      + B[2][0] * matrix_p[2][0])
+                  + A[1][2] * (B[0][0] * matrix_p[0][1]
+                      + B[1][0] * matrix_p[1][1] + B[2][0] * matrix_p[2][1]) + B[2][0] * matrix_p[2][2]},
+             {B[2][1] * matrix_p[2][0],
+              B[2][1] * matrix_p[2][1],
+              A[0][2] * B[2][1] * matrix_p[2][0] + A[1][2] * B[2][1] * matrix_p[2][1] + B[2][1] * matrix_p[2][2]}};
+        double matrix_inv_r_bt_pn_b[2][2] =
+            {{matrix_r_bt_pn_b[1][1]
+                  / (-matrix_r_bt_pn_b[0][1] * matrix_r_bt_pn_b[1][0]
+                      + matrix_r_bt_pn_b[0][0] * matrix_r_bt_pn_b[1][1]), -(matrix_r_bt_pn_b[0][1]
+                / (-matrix_r_bt_pn_b[0][1] * matrix_r_bt_pn_b[1][0]
+                    + matrix_r_bt_pn_b[0][0] * matrix_r_bt_pn_b[1][1]))},
+             {-(matrix_r_bt_pn_b[1][0]
+                 / (-matrix_r_bt_pn_b[0][1] * matrix_r_bt_pn_b[1][0]
+                     + matrix_r_bt_pn_b[0][0] * matrix_r_bt_pn_b[1][1])),
+              matrix_r_bt_pn_b[0][0] / (-matrix_r_bt_pn_b[0][1] * matrix_r_bt_pn_b[1][0]
+                  + matrix_r_bt_pn_b[0][0] * matrix_r_bt_pn_b[1][1])}};
+        double another_matrix[3][3] =
+            {{(matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][0]
+                + matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][0]
+                  + (matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][0],
+              (matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][1]
+                  + (matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][1],
+              (matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][2]
+                  + (matrix_at_pn_b[0][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[0][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][2]},
+             {(matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][0] +
+                 matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][0]
+                  + (matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][0],
+              (matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][1]
+                  + (matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][1],
+              (matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][2]
+                  + (matrix_at_pn_b[1][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[1][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][2]},
+             {(matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][0] +
+                 matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][0]
+                  + (matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][0],
+              (matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][1]
+                  + (matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][1],
+              (matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][0] +
+                  matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][0]) * matrix_bt_pn_a[0][2]
+                  + (matrix_at_pn_b[2][0] * matrix_inv_r_bt_pn_b[0][1]
+                      + matrix_at_pn_b[2][1] * matrix_inv_r_bt_pn_b[1][1]) * matrix_bt_pn_a[1][2]}};
+        matrix_p_next[0][0] = matrix_at_pn_a[0][0] - another_matrix[0][0] + Q[0][0];
+        matrix_p_next[0][1] = matrix_at_pn_a[0][1] - another_matrix[0][1];
+        matrix_p_next[0][2] = matrix_at_pn_a[0][2] - another_matrix[0][2];
+        matrix_p_next[1][0] = matrix_at_pn_a[1][0] - another_matrix[1][0];
+        matrix_p_next[1][1] = matrix_at_pn_a[1][1] - another_matrix[1][1] + Q[0][0];
+        matrix_p_next[1][2] = matrix_at_pn_a[1][2] - another_matrix[1][2];
+        matrix_p_next[2][0] = matrix_at_pn_a[2][0] - another_matrix[2][0];
+        matrix_p_next[2][1] = matrix_at_pn_a[2][1] - another_matrix[2][1];
+        matrix_p_next[2][2] = matrix_at_pn_a[2][2] - another_matrix[2][2] + Q[0][0];
+
+        double p_next_max_coefficient = fabs(matrix_p_next[0][0]);
+        for (unsigned char counter1 = 0; counter1 < 3; ++counter1)
+            for (unsigned char counter2 = 0; counter2 < 3; ++counter2)
+                if (p_next_max_coefficient < fabs(matrix_p_next[counter1][counter2]))
+                    p_next_max_coefficient = fabs(matrix_p_next[counter1][counter2]);
+
+        double p_max_coefficient = fabs(matrix_p[0][0]);
+        for (unsigned char counter1 = 0; counter1 < 3; ++counter1)
+            for (unsigned char counter2 = 0; counter2 < 3; ++counter2)
+                if (p_max_coefficient < fabs(matrix_p[counter1][counter2]))
+                    p_max_coefficient = fabs(matrix_p[counter1][counter2]);
+
+        if (fabs(p_max_coefficient - p_next_max_coefficient) < ITERATION_ACCURACY) {
+            printf("iteration counter:%d\r\n", counter);
+            break;
+        }
+
+        for (unsigned char counter1 = 0; counter1 < 3; ++counter1)
+            for (unsigned char counter2 = 0; counter2 < 3; ++counter2)
+                matrix_p[counter1][counter2] = matrix_p_next[counter1][counter2];
+    }
+
+    printf("------------Matrix P------------\r\n");
     for (int counter1 = 0; counter1 < 3; ++counter1) {
         for (int counter2 = 0; counter2 < 3; ++counter2)
-            printf("%f   ", A[counter1][counter2]);
-        printf("\r\n");
-    }
-
-    printf("\r\n------------Matrix B------------\r\n");
-    for (int counter1 = 0; counter1 < 3; ++counter1) {
-        for (int counter2 = 0; counter2 < 2; ++counter2)
-            printf("%f   ", B[counter1][counter2]);
-        printf("\r\n");
-    }
-
-    printf("\r\n------------Matrix Q------------\r\n");
-    for (int counter1 = 0; counter1 < 3; ++counter1) {
-        for (int counter2 = 0; counter2 < 3; ++counter2)
-            printf("%f   ", Q[counter1][counter2]);
-        printf("\r\n");
-    }
-
-    printf("\r\n------------Matrix R------------\r\n");
-    for (int counter1 = 0; counter1 < 2; ++counter1) {
-        for (int counter2 = 0; counter2 < 2; ++counter2)
-            printf("%f   ", R[counter1][counter2]);
+            printf("%f   ", matrix_p[counter1][counter2]);
         printf("\r\n");
     }
 }
--- testbench/script/solver_test.c
+++ testbench/script/solver_test.c
@@ -5,13 +5,13 @@
 #include "riccati_solver.h"
 
 void test_riccati_solver(void) {
-    double fai_r = 0.23, delta_r = 0.086, L = 0.4, v_r = 5.1, dt = 0.1;
-    double Matrix_A[3][3] = {{1, 0, -v_r * sin(fai_r)},
+    double fai_r = 0.23, delta_r = 0.086, L = 0.4, v_r = 5.1, dt = 0.01;
+    double Matrix_A[3][3] = {{1, 0, -v_r * dt * sin(fai_r)},
                              {0, 1, v_r * dt * cos(fai_r)},
                              {0, 0, 1}};
-    double Matrix_B[3][2] = {{cos(fai_r), 0},
-                             {sin(fai_r), 0},
-                             {tan(delta_r) / L, v_r / (L * cos(delta_r) * cos(delta_r))}};
+    double Matrix_B[3][2] = {{cos(fai_r) * dt, 0},
+                             {sin(fai_r) * dt, 0},
+                             {tan(delta_r) * dt / L, v_r * dt / (L * cos(delta_r) * cos(delta_r))}};
     double Matrix_Q[3][3] = {{1, 0, 0},
                              {0, 1, 0},
                              {0, 0, 1}};
