8a2d8be3a894948909f29a589bfe1bacddeed47e
zgl <1662568794@qq.com>
2022-02-24 23:08:45 +0800

    Passing imu's data inside main.c

--- program/proc_mcu/main.c
+++ program/proc_mcu/main.c
@@ -9,6 +9,7 @@
 #include "main.h"
 
 extern hmc5883l magnetometer;
+extern adis16470_t imu;
 
 int main(void) {
     delay_config();
@@ -27,18 +28,22 @@ int main(void) {
     xpt2046_gpio_config();
 //    xpt2046_calibrate();
     cm_backtrace_config("mm32f3277", "1.0.1", "1.0.1");
-    debugger_register_variable(dbg_uint32, &global_time_stamp, "time");
+//    debugger_register_variable(dbg_uint32, &global_time_stamp, "time");
+    debugger_register_variable(dbg_int16, &imu.x_acll, "imu.x_acll");
+    debugger_register_variable(dbg_int16, &imu.y_acll, "imu.y_acll");
+    debugger_register_variable(dbg_int16, &imu.z_acll, "imu.z_acll");
     timer2_config();
 
     while (1) {
 //        gui_show_gnrmc_information();       // 46.8ms
-//        adis_burst_read();
+
         iic_read_hmc5883l();
-        printf("x:%d, y:%d, z:%d\r\n", magnetometer.x, magnetometer.y, magnetometer.z);
+        printf("%d %d %d\r\n", imu.x_acll, imu.y_acll, imu.z_acll);
         delayms(50);
-        gui_printf(5, 0, C_BLACK, C_WHITE, "mag_x: %d   ", magnetometer.x);
-        gui_printf(5, 12, C_BLACK, C_WHITE, "mag_y: %d   ", magnetometer.y);
-        gui_printf(5, 24, C_BLACK, C_WHITE, "mag_z: %d   ", magnetometer.z);
+        adis_burst_read();
+        gui_printf(5, 0, C_BLACK, C_WHITE, "imu.x_acll: %d   ", imu.x_acll);
+        gui_printf(5, 12, C_BLACK, C_WHITE, "imu.y_acll: %d   ", imu.y_acll);
+        gui_printf(5, 24, C_BLACK, C_WHITE, "imu.z_acll: %d   ", imu.z_acll);
         gui_flush();            // 错开GUI的DMA刷新，但是UART6的DMA可能会受到这个的影响。
         delayms(50);
     }
