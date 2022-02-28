c08b58c10385a3e74605f18ecd5e6235606822e0
zgl <1662568794@qq.com>
2022-03-01 00:15:30 +0800

    Kalman fusion complete

--- program/math/kalman.c
+++ program/math/kalman.c
@@ -30,6 +30,9 @@ float kalman_update(kalman_filter_float *kalman, float newpos, float newVel, flo
     kalman->vel = newVel - kalman->bias;
     kalman->pos += dt * kalman->vel;
 
+    kalman->pos =
+        (kalman->pos < 0) ? kalman->pos + 360.0f : (kalman->pos > 360.0f) ? kalman->pos - 360.0f : kalman->pos;
+
     // Update estimation error covariance - Project the error covariance ahead
     /* Step 2 */
     kalman->P[0][0] += dt * (dt * kalman->P[1][1] - kalman->P[0][1] - kalman->P[1][0] + kalman->QPos);
@@ -63,71 +66,6 @@ float kalman_update(kalman_filter_float *kalman, float newpos, float newVel, flo
     kalman->P[0][1] -= K[0] * P01_temp;
     kalman->P[1][0] -= K[1] * P00_temp;
     kalman->P[1][1] -= K[1] * P01_temp;
-
-    return kalman->pos;
-};
-
-void kalman_config_int(kalman_filter_int *kalman) {
-    kalman->QPos = (int) (0.001 * PRECISION_FLOAT_TO_INT);
-    kalman->QVel = (int) (0.003 * PRECISION_FLOAT_TO_INT);
-    kalman->R = (int) (0.03 * PRECISION_FLOAT_TO_INT);
-
-    kalman->pos = 0;  // Reset the pos
-    kalman->vel = 0;  // Reset speed
-    kalman->bias = 0; // Reset bias of velocity
-
-    // Since we assume that the speed is 0 and we know the starting pos (use setpos),
-    // the error covariance matrix is set like so
-    kalman->P[0][0] = (int) (0.0049525 * PRECISION_FLOAT_TO_INT);
-    kalman->P[0][1] = (int) (0.0032382 * PRECISION_FLOAT_TO_INT);
-    kalman->P[1][0] = (int) (0.0032382 * PRECISION_FLOAT_TO_INT);
-    kalman->P[1][1] = (int) (0.0048883 * PRECISION_FLOAT_TO_INT);
-};
-
-/* 单位：pos：0.001°；w：0.001°/s；dt：ms */
-/* 原本dt是0.1s，将其转化为频率就是10Hz， */
-int kalman_update_int(kalman_filter_int *kalman, int newpos, int newVel, int f) {
-
-    // Discrete Kalman filter time update equations - Time Update ("Predict")
-    // Update xhat - Project the state ahead
-    /* Step 1 */
-    kalman->vel = newVel - kalman->bias;
-    kalman->pos += kalman->vel / f;
-
-    // Update estimation error covariance - Project the error covariance ahead
-    /* Step 2 */
-    kalman->P[0][0] += (kalman->P[1][1] / f - kalman->P[0][1] - kalman->P[1][0] + kalman->QPos) / f;
-    kalman->P[0][1] -= kalman->P[1][1] / f;
-    kalman->P[1][0] -= kalman->P[1][1] / f;
-    kalman->P[1][1] += kalman->QVel / f;
-
-    // Calculate pos and velocity - Update estimate with measurement zk (newpos)
-    /* Step 3 */
-    int y = newpos - kalman->pos; // pos difference
-
-    // Discrete Kalman filter measurement update equations - Measurement Update ("Correct")
-    // Calculate Kalman gain - Compute the Kalman gain
-    /* Step 4 */
-    int S = kalman->P[0][0] + kalman->R; // Estimate error
-    /* Step 5 */
-    int K[2]; // Kalman gain - This is a 2x1 vector
-    K[0] = kalman->P[0][0] * PRECISION_FLOAT_TO_INT / S;
-    K[1] = kalman->P[1][0] * PRECISION_FLOAT_TO_INT / S;
-
-
-    /* Step 6 */
-    kalman->pos += K[0] * y / PRECISION_FLOAT_TO_INT / PRECISION_FLOAT_TO_INT;
-    kalman->vel += K[1] * y / PRECISION_FLOAT_TO_INT / PRECISION_FLOAT_TO_INT;   // ?
-
-    // Calculate estimation error covariance - Update the error covariance
-    /* Step 7 */
-    int P00_temp = kalman->P[0][0];
-    int P01_temp = kalman->P[0][1];
-
-    kalman->P[0][0] -= K[0] * P00_temp / PRECISION_FLOAT_TO_INT;
-    kalman->P[0][1] -= K[0] * P01_temp / PRECISION_FLOAT_TO_INT;
-    kalman->P[1][0] -= K[1] * P00_temp / PRECISION_FLOAT_TO_INT;
-    kalman->P[1][1] -= K[1] * P01_temp / PRECISION_FLOAT_TO_INT;
 
     return kalman->pos;
 };
