9738ff7d7f4d7d08ee0522bcdfeb7028241e9aec
LaoZhu <2208213223@qq.com>
2022-03-07 00:45:45 +0800

    Try to solve the problem of running unit test script generation errors on github.

--- testbench/unit_test.c
+++ testbench/unit_test.c
@@ -1,22 +1,27 @@
 #include "main.h"
 
 int generate_statements_report(void) {
-    /* Run all tests using the CUnit Basic interface */
-    CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
-
+#ifdef CMAKE_GITHUB_BUILD
     /* Run all tests using the CUnit Automatic interface */
     CU_set_output_filename("Report");
     CU_list_tests_to_file();
     CU_automated_run_tests();
-
+    printf("Run all tests using the CUnit Automatic interface.\r\n");
+#else
+    /* Run all tests using the CUnit Basic interface */
+    CU_basic_set_mode(CU_BRM_VERBOSE);
+    CU_basic_run_tests();
+    printf("Run all tests using the CUnit Basic interface.\r\n");
+#endif
     CU_cleanup_registry();
+    printf("Clean up all registry.\r\n");
     return CU_get_error();
 }
 
 int initialize_cunit_suite(CU_pSuite *psuite_ptr, const char *strName) {
     static int initialize_registry_flag = 0;
     if (initialize_registry_flag == 0) {
+        printf("Initialize redistry.\r\n");
         /* initialize the CUnit test registry */
         if (CUE_SUCCESS != CU_initialize_registry())
             return CU_get_error();
@@ -27,6 +32,7 @@ int initialize_cunit_suite(CU_pSuite *psuite_ptr, const char *strName) {
     *psuite_ptr = CU_add_suite(strName, NULL, NULL);
     if (NULL == *psuite_ptr) {
         CU_cleanup_registry();
+        printf("suite add failed.\r\n");
         return CU_get_error();
     }
     return 0;
@@ -36,6 +42,7 @@ int add_test_scripts_to_suite(CU_pSuite *psuite_ptr, const char *strName, CU_Tes
     /* add the tests to the suite */
     if (NULL == CU_add_test(*psuite_ptr, strName, pTestFunc)) {
         CU_cleanup_registry();
+        printf("test scripts add failed.\r\n");
         return CU_get_error();
     }
     return 0;
