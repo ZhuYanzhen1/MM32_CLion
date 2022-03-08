8a8514569a589f2da3cd4ad5bb3c8ff1272ab125
zgl <1662568794@qq.com>
2022-03-09 00:41:00 +0800

    Verify the feasibility of geomagnetometer correction success

--- program/math/mag_calibrate.h
+++ program/math/mag_calibrate.h
@@ -1,11 +1,11 @@
-typedef struct{
-	unsigned char vflg; 
-	float offset[3];  //bias error
+typedef struct {
+    unsigned char vflg;
+    float offset[3];  //bias error
     float diag[3];    //scale-factor error
     float offdiag[3]; //cross-axis sensitivity
     float radius;     //sphere radius
 } calpara_t;
 
-#define COMPASS_CAL_NUM_SAMPLES 2048   //总采样数
+#define COMPASS_CAL_NUM_SAMPLES 500   //总采样数
 
 char CompassCal(float sample[3]);
