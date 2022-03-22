cac2cbbc361b7db77515875d0e0d49733573d2ac
zgl <1662568794@qq.com>
2022-03-22 18:18:36 +0800

    Successful porting of the modified Kalman fusion scheme to rtthread

--- program/device/sensor_decode.c
+++ program/device/sensor_decode.c
@@ -111,7 +111,7 @@ void unpacking_fixed_length_data(unsigned char packets[10]) {
             packets[i] = 0xff;
 
     small_packets.north = (short) (packets[0] | (((short) packets[5] & 0xf0) << 4));
-    small_packets.kalman_north = (short) (packets[1] | (((short) packets[5] & 0x0f) << 8));
+    small_packets.kalman_north = (float) ((short) (packets[1] | (((short) packets[5] & 0x0f) << 8))) / 10.0f;
     small_packets.ax = (short) (packets[2] | (((short) packets[6] & 0xf0) << 4));
     small_packets.ay = (short) (packets[3] | ((short) (packets[6] & 0x0f) << 8));
     small_packets.az = (short) (packets[4] | ((short) (packets[7] & 0xf0) << 4));
