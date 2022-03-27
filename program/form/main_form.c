4810950a886b31030af0f46661a8db41f20d5e57
LaoZhu <2208213223@qq.com>
2022-03-27 19:53:10 +0800

    Reprogram and rewrite the main form.

--- program/form/main_form.c
+++ program/form/main_form.c
@@ -3,22 +3,27 @@
 //
 
 #include "main.h"
-#include "fusion_form.h"
-#include "mag_calibrate_from.h"
 
 form_struct_t main_form;
+static button_struct_t origin_form_btn;
 static button_struct_t fusion_form_btn;
-static button_struct_t mag_calibrate_form_btn;
+static button_struct_t calibrate_form_btn;
+static button_struct_t debug_form_btn;
 
-void mag_calibrate_form_btn_callback(void *parameter) {
+void origin_form_btn_callback(void *parameter) {
     (void) parameter;
-    gui_mag_calibrate_from_init();
-    uart8_sendbyte(1);
 }
 
 void fusion_form_btn_callback(void *parameter) {
     (void) parameter;
-    gui_fusion_form_init();
+}
+
+void calibrate_form_btn_callback(void *parameter) {
+    (void) parameter;
+}
+
+void debug_form_btn_callback(void *parameter) {
+    (void) parameter;
 }
 
 void gui_main_form_update(void *parameter) {
@@ -26,14 +31,21 @@ void gui_main_form_update(void *parameter) {
 }
 
 void gui_main_form_init(void) {
+    origin_form_btn.callback = origin_form_btn_callback;
     fusion_form_btn.callback = fusion_form_btn_callback;
-    mag_calibrate_form_btn.callback = mag_calibrate_form_btn_callback;
-    gui_button_init(&fusion_form_btn, 10, 120, 49, 30, "Fusion");
-    gui_button_init(&mag_calibrate_form_btn, 10, 80, 63, 30, " Calibrate");
+    calibrate_form_btn.callback = calibrate_form_btn_callback;
+    debug_form_btn.callback = debug_form_btn_callback;
+
+    gui_button_init(&origin_form_btn, 14, 18 + 6, 100, 26, " Origin Data");
+    gui_button_init(&fusion_form_btn, 14, 50 + 6, 100, 26, "Fusion Data");
+    gui_button_init(&calibrate_form_btn, 14, 82 + 6, 100, 26, "Calibrate");
+    gui_button_init(&debug_form_btn, 14, 114 + 6, 100, 26, " Debug Mode");
 
     gui_form_init(&main_form, "MainForm", gui_main_form_update);
 
+    gui_form_bind_button(&main_form, &origin_form_btn);
     gui_form_bind_button(&main_form, &fusion_form_btn);
-    gui_form_bind_button(&main_form, &mag_calibrate_form_btn);
+    gui_form_bind_button(&main_form, &calibrate_form_btn);
+    gui_form_bind_button(&main_form, &debug_form_btn);
     gui_form_display(&main_form);
 }
