53a1ce3fdb072b4e6f9ca452bea087f984251e5e
LaoZhu <2208213223@qq.com>
2022-03-27 18:01:48 +0800

    Add return button in fusion form.

--- program/form/fusion_form.c
+++ program/form/fusion_form.c
@@ -5,6 +5,7 @@
 #include "main.h"
 #include "fusion_form.h"
 
+extern form_struct_t main_form;
 extern kalman_data_t kalman_data;
 
 static form_struct_t fusion_form;
@@ -17,18 +18,24 @@ static label_struct_t kalman_v_lable;
 static label_struct_t neu_v_lable;
 static label_struct_t kalman_north_distance_lable;
 static label_struct_t kalman_east_distance_lable;
+static button_struct_t fusion_form_return_btn;
+
+void fusion_form_return_btn_callback(void *parameter) {
+    (void) parameter;
+    gui_form_display(&main_form);
+}
 
 void gui_show_fusion(void *parameter) {
     (void) parameter;
-    gui_label_settext(&north_label, " north:%f   ", small_packets.north);
-    gui_label_settext(&kalman_north_label, " k_north:%f   ", small_packets.chebyshev_north);
-    gui_label_settext(&gnss_status_label, " status:%c ", gps_rmc.status);
-    gui_label_settext(&north_distance_label, " nd:%f  ", neu.north_distance);
-    gui_label_settext(&east_distance_label, " ed:%f  ", neu.east_distance);
-    gui_label_settext(&kalman_v_lable, " k_v:%f  ", kalman_data.v);
-    gui_label_settext(&neu_v_lable, " v:%f  ", neu.v);
-    gui_label_settext(&kalman_north_distance_lable, " k_nd:%f  ", kalman_data.distance_north);
-    gui_label_settext(&kalman_east_distance_lable, " k_ed:%f  ", kalman_data.distance_east);
+    gui_label_settext(&north_label, " north:%.2f", small_packets.north);
+    gui_label_settext(&kalman_north_label, " k_north:%.2f", small_packets.chebyshev_north);
+    gui_label_settext(&gnss_status_label, " status:%c", gps_rmc.status);
+    gui_label_settext(&north_distance_label, " nd:%.3f", neu.north_distance);
+    gui_label_settext(&east_distance_label, " ed:%.3f", neu.east_distance);
+    gui_label_settext(&kalman_v_lable, " k_v:%f", kalman_data.v);
+    gui_label_settext(&neu_v_lable, " v:%f", neu.v);
+    gui_label_settext(&kalman_north_distance_lable, " k_nd:%.3f", kalman_data.distance_north);
+    gui_label_settext(&kalman_east_distance_lable, " k_ed:%.3f", kalman_data.distance_east);
 }
 
 void gui_fusion_form_init() {
@@ -42,6 +49,9 @@ void gui_fusion_form_init() {
     gui_label_init(&kalman_north_distance_lable, 7, C_BLACK, label_align_left, " k_nd");
     gui_label_init(&kalman_east_distance_lable, 8, C_BLACK, label_align_left, " k_ed");
 
+    fusion_form_return_btn.callback = fusion_form_return_btn_callback;
+    gui_button_init(&fusion_form_return_btn, 68, 129, 50, 24, "Back");
+
     gui_form_init(&fusion_form, "FusionForm", gui_show_fusion);
 
     gui_form_bind_label(&fusion_form, &north_label);
@@ -53,5 +63,6 @@ void gui_fusion_form_init() {
     gui_form_bind_label(&fusion_form, &neu_v_lable);
     gui_form_bind_label(&fusion_form, &kalman_north_distance_lable);
     gui_form_bind_label(&fusion_form, &kalman_east_distance_lable);
+    gui_form_bind_button(&fusion_form, &fusion_form_return_btn);
     gui_form_display(&fusion_form);
 }
