ea335be78e776b8786800fe9c47fc82c0c5b5104
zgl <1662568794@qq.com>
2022-04-23 03:19:02 +0800

    Separate the LQR control for unit tests from ctrl_mcu

--- testbench/script/solver_test.c
+++ testbench/script/solver_test.c
@@ -3,10 +3,14 @@
 
 #include "math.h"
 #include "riccati_solver.h"
-
 #include "mag_calibrate.h"
+#include "fast_math.h"
 #include "stdio.h"
 #include "string.h"
+#include "test_data.h"
+
+#define TOLERANCE_PRECISION     0.2f
+#define YAW_TO_ANGLE            (-60.3111346f)     // 180/pi * (-50/47.5)
 
 extern calpara_t params;
 
@@ -52,7 +56,58 @@ void test_calibration_solver(void) {
     }
 }
 
-#define TOLERANCE_PRECISION     0.2f
+float distance_north;
+float distance_east;
+
+float calculate_distance(int ind) {
+    float distance = sqrtf(
+            ((test_point_unit[ind][0] - distance_north) * (test_point_unit[ind][0] - distance_north)
+             + (test_point_unit[ind][1] - distance_east) * (test_point_unit[ind][1] - distance_east)));
+    return distance;
+}
+
+void lqr_control_test(unsigned short index, int angle_ctrl, float angle_yaw, float distance_n, float distance_e) {
+
+    float v_r = 2, dt = 0.02f, L = 0.3f;
+
+    // 求位置、航向角的误差
+    float yaw_temp = (angle_yaw < 180) ? angle_yaw : (180 - angle_yaw);
+    yaw_temp *= (0.0174533f);
+    float x_error = distance_n - test_point_unit[index][0];
+    float y_error = distance_e - test_point_unit[index][1];
+    float yaw_error = yaw_temp - test_point_unit[index][2];
+
+    // 计算横向误差
+    float lateral_error = y_error * (test_point_unit[index][2]) - x_error * my_asin(test_point_unit[index][2]);
+
+    // 由状态方程矩阵系数，计算K
+    float a[3][3] = {{1, 0, -v_r * dt * sinf(test_point_unit[index][2])},
+                     {0, 1, v_r * dt * cosf(test_point_unit[index][2])},
+                     {0, 0, 1}};
+    float b[3][2] = {{cosf(test_point_unit[index][2]) * dt, 0},
+                     {sinf(test_point_unit[index][2]) * dt, 0},
+                     {tanf(yaw_error) * dt / L,             v_r * dt /
+                                                            (L * cosf(yaw_error) * cosf(yaw_error))}};
+
+    // 获得速度误差量、前轮转角误差量两个控制量
+    float x[3][1] = {{x_error},
+                     {y_error},
+                     {yaw_error}};
+    float p[3][3] = {0};
+    float control_val[2][1] = {0};
+    float r = 1;
+    float q = 1;
+
+    solve_riccati_equation(a, b, q, r, p);
+    solve_feedback_value(p, a, b, x, r, control_val);
+//    speed = speed +control_val[0][0];
+    short delta_angle = (short) (control_val[1][0] * YAW_TO_ANGLE);
+    angle_ctrl = angle_ctrl + delta_angle;
+    if (angle_ctrl > 200)
+        angle_ctrl = 200;
+    else if (angle_ctrl < 100)
+        angle_ctrl = 100;
+}
 
 void test_riccati_solver(void) {
     float fai_r = 0.23f, delta_r = 0.086f, L = 0.4f, v_r = 5.1f, dt = 0.01f;
@@ -61,8 +116,8 @@ void test_riccati_solver(void) {
                      {0, 1, v_r * dt * cosf(fai_r)},
                      {0, 0, 1}};
 
-    float b[3][2] = {{cosf(fai_r) * dt, 0},
-                     {sinf(fai_r) * dt, 0},
+    float b[3][2] = {{cosf(fai_r) * dt,       0},
+                     {sinf(fai_r) * dt,       0},
                      {tanf(delta_r) * dt / L, v_r * dt / (L * cosf(delta_r) * cosf(delta_r))}};
     float x[3][1] = {{error_x},
                      {error_y},
@@ -81,35 +136,15 @@ void test_riccati_solver(void) {
     CU_ASSERT(fabsf(control_val[1][0]) > fabsf(-0.3026f + TOLERANCE_PRECISION))
 }
 
-extern float distance_north;
-extern float distance_east;
-
-void test_calc_target_index(void) {
-    int ind;
-
-//    {23.164415f, 140.739357f},
-//    25.032100f, 140.478674f
-    ind = 137;
-    distance_north = 23.463922f;
-    distance_east = 140.948782f; // 注：修改了窗口 和 计算距离的函数内存放位置点的数据
-//    ind_ahead = (int) newton_iteration((ind + INDEX_OFFSET > 412) ? 412 : (ind + INDEX_OFFSET));
-//    ind_rear = (int) newton_iteration((ind - INDEX_OFFSET < 0) ? 0 : (ind - INDEX_OFFSET));
-    ind = dichotomy(ind - 2, ind + 20);
-    CU_ASSERT_EQUAL(ind, 138)
-
-}
-
 void test_lqr_contrl(void) {
     short angle_ctrl = 150;
     unsigned short index;
     float distance_n, distance_e, angle_yaw;
 
-    index = 16, distance_n = 337764.781f, distance_e = 347232.688f, angle_yaw =
-        242.7f; // 337766.6207556f, 347232.7255131f
+    index = 16, distance_n = 337764.781f, distance_e = 347232.688f, angle_yaw = 242.7f; // 337766.6207556f, 347232.7255131f
     lqr_control_test(index, angle_ctrl, angle_yaw, distance_n, distance_e);
 
-    index = 15, distance_n = 337765.781f, distance_e = 347231.938f, angle_yaw =
-        147.5f; // 337766.6029507f, 347232.4832368f
+    index = 15, distance_n = 337765.781f, distance_e = 347231.938f, angle_yaw = 147.5f; // 337766.6029507f, 347232.4832368f
     lqr_control_test(index, angle_ctrl, angle_yaw, distance_n, distance_e);
 
 }
