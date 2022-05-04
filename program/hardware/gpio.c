9570e26b72b1fc6e56e9ae66c5ec97f1f08255b2
LaoZhu <2208213223@qq.com>
2022-05-05 00:27:28 +0800

    Added ch372 initialization, but the communication timing is still problematic.

--- program/hardware/gpio.c
+++ program/hardware/gpio.c
@@ -9,13 +9,65 @@
 #include "gpio.h"
 #ifdef IS_CONTROL_MCU
 #include "../ctrl_mcu/config.h"
+#include "../ctrl_mcu/pin.h"
 #else
 #include "../proc_mcu/config.h"
+#include "../proc_mcu/pin.h"
 #endif  // IS_CONTROL_MCU
 #include "hal_conf.h"
 #include "mm32_device.h"
-#include "pin.h"
 
+#ifdef IS_CONTROL_MCU
+
+void ch372c_gpio_config(void) {
+    GPIO_InitTypeDef GPIO_InitStruct;
+    NVIC_InitTypeDef NVIC_InitStruct;
+    EXTI_InitTypeDef EXTI_InitStruct;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2ENR_SYSCFG, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOC, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOE, ENABLE);
+    RCC_AHBPeriphClockCmd(RCC_AHBENR_GPIOF, ENABLE);
+
+    GPIO_StructInit(&GPIO_InitStruct);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6
+        | GPIO_Pin_7 | GPIO_Pin_8 | GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_OD;
+    GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+    GPIO_SetBits(CH372_RD_PORT, CH372_RD_PIN);
+    GPIO_SetBits(CH372_A0_PORT, CH372_A0_PIN);
+    GPIO_SetBits(CH372_CS_PORT, CH372_CS_PIN);
+    GPIO_SetBits(CH372_WR_PORT, CH372_WR_PIN);
+    GPIO_SetBits(GPIOF, 0x07f8);
+
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOE, EXTI_PinSource6);
+    EXTI_InitStruct.EXTI_Line = EXTI_Line6;
+    EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+    EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+    EXTI_Init(&EXTI_InitStruct);
+
+    NVIC_InitStruct.NVIC_IRQChannel = EXTI9_5_IRQn;
+    NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = EXTI6_PRIORITY;
+    NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0;
+    NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&NVIC_InitStruct);
+}
+
+#endif
 #ifdef IS_PROCESS_MCU
 void lcd_gpio_config(void) {
     GPIO_InitTypeDef GPIO_InitStruct;
