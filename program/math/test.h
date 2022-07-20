17512809ace63a6037da067620aeb01af4e1f048
zgl <1662568794@qq.com>
2022-07-20 21:17:48 +0800

    2w .. path_8

--- program/math/test.h
+++ program/math/test.h
@@ -5,9 +5,9 @@
 #ifndef MM32F3277_MATH_TEST_H_
 #define MM32F3277_MATH_TEST_H_
 
-#define INDEX_NUM   709  // test_point_1：641（体育馆）    test_point_2：671（国防生）  第五道
-#define FIRST_DIS_N 4392.4940000f  // 4392.4940000f, 39677.2340000f
-#define FIRST_DIS_E 39677.2340000f
+#define INDEX_NUM   630  // test_point_1：641（体育馆）    test_point_2：671（国防生）  第五道
+#define FIRST_DIS_N 4388.9400000f  // 4388.9400000f, 39674.8910000f
+#define FIRST_DIS_E 39674.8910000f
 //#define test_point  test_point_2
 
 extern float test_point[][4];
