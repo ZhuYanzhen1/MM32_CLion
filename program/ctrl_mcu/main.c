8b9cb9508d8d80fa8e5fcb2c289def26caa05e54
LaoZhu <2208213223@qq.com>
2022-07-30 19:52:20 +0800

    Add comments to controlMCU main.c program

--- program/ctrl_mcu/main.c
+++ program/ctrl_mcu/main.c
@@ -8,31 +8,39 @@
 
 #include "main.h"
 
+/*! \brief FAT file system structure */
 FATFS filesystem;
+
+/*! \brief File system buffer arrays */
 static unsigned char fs_buffer[FF_MAX_SS * 4];
 
-/*! \brief  Determining whether to perform LQR control */
+/*! \brief Determining whether to perform LQR control */
 extern volatile unsigned char lqr_flag;
 
-/*! \brief  Motor control volume */
+/*! \brief Motor expect speed */
 volatile unsigned short speed = 0;
 
-/*! \brief  The nearest track point to yourself */
+/*! \brief The nearest reference point to the vehicle */
 static volatile unsigned short playground_ind = 0;
 
-/*! \brief  Information received from proc_mcu */
+/*! \brief Information received from proc_mcu */
 extern unsigned int uart6_dma_buffer_1[CTRL_MCU_RECEIVE_AMOUNT];
 extern unsigned int uart6_dma_buffer_2[CTRL_MCU_RECEIVE_AMOUNT];
 
-/*! \brief  Temperature of the motor */
+/*! \brief Temperature of the motor */
 extern unsigned char temperature;
 
-/*! \brief  Messages sent to the brushless control board */
+/*! \brief Messages sent to the brushless driver board */
 unsigned int uart7_dma_send_buffer[UART7_DMA_SEND_BUFFER] = {0};
 
 int main(void) {
+    /* Initialize systick system clock for time delay */
     delay_config();
+
+    /* Initialize the PWM GPIO for timer3 */
     tim3_pwm_gpio_config();
+
+    /* Initialize other peripherals for communication */
     led_config();
     uart1_config();
     uart6_config();
@@ -42,17 +50,24 @@ int main(void) {
     uart7_config();
     uart7_dma_sent_config(uart7_dma_send_buffer, UART7_DMA_SEND_BUFFER);
 
+    /* Delay to wait for the radio transmission module to be ready */
     delayms(1000);
+
+    /* Initialize debugger-related structures and print debugging information */
     cm_backtrace_config("CtrlMCU", "1.6.1", "1.6.1");
     debugger_register_variable(dbg_uint32, &global_time_stamp, "time");
     printf("CtrlMCU %s %s %s\r\n", __DATE__, __TIME__, GIT_HASH);
     timer2_config();
 
+    /* Initialize Timer 3 and SPI3 and start PWM output */
     spi3_config();
     delayms(2000);
     timer3_config();
 
+    /* Switch SD card read/write interface to MCU */
     sdcard_switch_device(0);
+
+    /* Try to mount the SD card to the file system */
     FRESULT result = f_mount(&filesystem, "0:", 1);
     if (result == FR_NO_FILESYSTEM) {
         printf("Making FileSystem...\r\n");
@@ -68,19 +83,29 @@ int main(void) {
         while (1);
     }
     printf("FileSystem Mount Success!\r\n");
+
+    /* Write the current firmware information and scan files in SD card*/
     fs_write_current_info();
     fs_scan_files("0:");
 
+    /* Define the variables needed to find the closest point at departure */
     static unsigned char find_counter = 0;
     static unsigned short start_point = 0;
     volatile static unsigned short index = 0;
 
-    while (1) { // 寻点稳定再发车
+    /* Find the closest reference point to the car at the time of departure */
+    while (1) {
         LED1_TOGGLE();
         delayms(500);
+
+        /* Transferring motor temperature information to ProcMCU */
         uart6_sendbyte(temperature);
+
+        /* Determine whether the positioning is stable */
         if (proc_data.distance_east != 0 && lqr_flag == 1) {
             lqr_flag = 0;
+
+            /* Find the nearest reference point to the current point */
             playground_ind = find_index(playground_ind);
             start_point = playground_ind;
             find_counter++;
@@ -90,35 +115,46 @@ int main(void) {
         }
     }
 
+    /* Start control, start the car */
     while (1) {
+        /* Ensure that the current positioning information is valid */
         if (proc_data.distance_east != 0) {
+            /* 10 cycles of servo control with a 400ms cycle */
             for (unsigned char i = 0; i < 10; i++) {
                 while (1) {
                     if (lqr_flag == 1) {
                         lqr_flag = 0;
                         LED1_TOGGLE();
 
+                        /* Load the current information into the structure */
                         basic_status_t current_status = {proc_data.distance_north,
                                                          proc_data.distance_east,
                                                          proc_data.north_angle};
 
+                        /* Find the nearest reference point to the current point */
                         playground_ind = find_index(playground_ind);
 
+                        /* Override the reference point and feed it to the LQR controller to calculate the servo angle */
                         index = playground_ind + OVERRUN_POINT;
                         unsigned char angle = lqr_control(index, current_status);
 
+                        /* Write the servo angle control amount to the comparator */
                         WRITE_REG(TIM3->CCR1, angle);
                         break;
                     }
                 }
             }
-            speed = 23000;  // 23000
+            /* Set the motor speed, and set the speed to 0 if the end point is reached */
+            speed = 23000;
             if (playground_ind > INDEX_NUM - 10)
                 speed = 0;
+
+            /* Sending motor speed to the motor driver */
             sdtp_data_transmit_speed(speed, uart7_dma_send_buffer);
             uart7_dma_set_send_buffer(uart7_dma_send_buffer, UART7_DMA_SEND_BUFFER);
+
+            /* Transferring motor temperature information to ProcMCU */
             uart6_sendbyte(temperature);
-            printf("%.3f, %.3f\r\n", proc_data.distance_north, proc_data.distance_east);
         }
     }
 }
